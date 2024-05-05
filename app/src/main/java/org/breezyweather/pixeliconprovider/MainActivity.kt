5913d85063449a3c47f258b1fc9b1b35a953a7c9
Moonji <82398591+M00NJ@users.noreply.github.com>
2024-05-05 19:28:58 +0200

    Update MainActivity.kt

--- app/src/main/java/org/breezyweather/pixeliconprovider/MainActivity.kt
+++ app/src/main/java/org/breezyweather/pixeliconprovider/MainActivity.kt
@@ -1,20 +1,20 @@
 /**
- * This file is part of Breezy Weather Pixel Icon Provider.
+ * This file is part of Material Icon Provider.
  *
- * Breezy Weather Pixel Icon Provider is free software: you can redistribute it and/or modify it
+ * Material Icon Provider is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
  * Free Software Foundation, version 3 of the License.
  *
- * Breezy Weather Pixel Icon Provider is distributed in the hope that it will be useful, but
+ * Material Icon Provider is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Breezy Weather Pixel Icon Provider. If not, see <https://www.gnu.org/licenses/>.
+ * along with Material Icon Provider. If not, see <https://www.gnu.org/licenses/>.
  */
 
-package org.breezyweather.pixeliconprovider
+package com.moonj.materialiconprovider
 
 import android.app.Activity
 import android.os.Bundle
